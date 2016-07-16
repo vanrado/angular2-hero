0b5765f3be8929d6eb178f232b71c8d40feec9ba
U-ISW\racak <racak@racak.isw.local>
2016-07-16 20:12:56 +0200

    promise service setTimeout 2000ms

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {MOCK_HEROES} from "./mock-heroes";
+import {Hero} from "./models/Hero";
 
 @Injectable()
 export class HeroService {
   getHeroes() {
-    return Promise.resolve(MOCK_HEROES);
+    // return Promise.resolve(MOCK_HEROES);
+    return new Promise<Hero[]>(resolve =>
+      setTimeout(() => resolve(MOCK_HEROES), 2000)
+    );
   }
 }
